f3ea9c85dc16b189e48abdecd30db14cd78afd8a
祐介 戸田 <k022g0025@m.neec.ac.jp>
2023-09-05 16:19:19 +0900

    必要なヘッダーファイルを用意

---
+++ Class/Map/Map.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Map {
+public: // メンバ関数
+	void Initialize();	// 初期化
+	void Update();	// 更新
+	void Draw();	// 描画
+
+
+private: // メンバ変数
+};
--- Class/Models/Block.h
+++ Class/Models/Block.h
@@ -8,5 +8,9 @@ public: // メンバ関数
 	void Draw();	// 描画
 
 private: // メンバ変数
+
+	// モデルのポインタ
 	ModelData* modelData_;
+	// ワールドトランスフォーム
+	//WorldTransform worldTransform_;
 };
